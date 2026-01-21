2fd73ae08e1815f27e65d52d4d5acd1dce1e4acf
Sankalp Jha <sankalp.jha9643@gmail.com>
2026-01-21 21:02:28 +0530

    Update and rename connect4.go to main.go

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	ReadmeFile  = "README.md"
+	StartMarker = ""
+	EndMarker   = ""
+	ApiURL      = "https://icanhazdadjoke.com/"
+)
+
+type JokeResponse struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
+
+func main() {
+	// 1. Fetch the Joke
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", ApiURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// This header is required to get JSON instead of HTML
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", "GitHub Readme Bot (https://github.com/blackdragoon26/blackdragoon26)")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	var data JokeResponse
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Fetched joke:", data.Joke)
+
+	// 2. Read README
+	contentBytes, err := os.ReadFile(ReadmeFile)
+	if err != nil {
+		panic(err)
+	}
+	content := string(contentBytes)
+
+	// 3. Find Markers
+	startIndex := strings.Index(content, StartMarker)
+	endIndex := strings.Index(content, EndMarker)
+
+	if startIndex == -1 || endIndex == -1 {
+		fmt.Println("Markers not found in README.md")
+		os.Exit(1)
+	}
+
+	// 4. Construct New Content
+	// We keep the markers so the script can run again tomorrow
+	newSection := fmt.Sprintf("%s\n### Worst Dad Joke of the day:\n> %s\n%s", StartMarker, data.Joke, EndMarker)
+
+	// Replace the old part with the new part
+	newContent := content[:startIndex] + newSection + content[endIndex+len(EndMarker):]
+
+	// 5. Write Back
+	err = os.WriteFile(ReadmeFile, []byte(newContent), 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("README updated successfully.")
+}
